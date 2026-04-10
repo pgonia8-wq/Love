163823785ac37e54f16d22bb7adf6655f075e424
Hbyhumans <pgonia8@gmail.com>
2026-04-09 18:39:57 -0600

    Wrap app with MiniKitProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import MiniKitProvider from "./components/minikit-provider";
 
-import { MiniKit } from "@worldcoin/minikit-js";
+console.log("[H Love] App starting...");
 
-const appId = import.meta.env.VITE_WORLD_APP_ID || "app_ccf542f4e61d9faa92be78b5154299b4";
-
-console.log("[H Love] Initializing MiniKit with app_id:", appId);
-
-MiniKit.install(appId);
-
-console.log("[H Love] MiniKit installed:", MiniKit.isInstalled());
-
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <MiniKitProvider>
+    <App />
+  </MiniKitProvider>
+);
