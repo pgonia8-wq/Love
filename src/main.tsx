de6117b7a0e1b277e7f21c135d56470953e7a67e
Hbyhumans <pgonia8@gmail.com>
2026-04-09 18:15:15 -0600

    Fix: install MiniKit at app startup with app_id

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+import { MiniKit } from "@worldcoin/minikit-js";
+
+const appId = import.meta.env.VITE_WORLD_APP_ID || "app_ccf542f4e61d9faa92be78b5154299b4";
+
+console.log("[H Love] Initializing MiniKit with app_id:", appId);
+
+MiniKit.install(appId);
+
+console.log("[H Love] MiniKit installed:", MiniKit.isInstalled());
+
 createRoot(document.getElementById("root")!).render(<App />);
